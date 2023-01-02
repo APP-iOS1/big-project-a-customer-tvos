c33ce8a9541afaa3281c492320b0a6f83726a2dc
gxdxt <yxxsxxny@gmail.com>
2023-01-02 13:53:36 +0900

    tvOS init

---
+++ AuChulTV/AuChulTV/AuChulTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuChulTVApp.swift
+//  AuChulTV
+//
+//  Created by tae on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct AuChulTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuChulTV/AuChulTV/Model/NotebookItem.swift
@@ -0,0 +1,27 @@
+//
+//  NotebookItem.swift
+//  AuChulTV
+//
+//  Created by tae on 2023/01/02.
+//
+
+import Foundation
+
+
+struct NotebookItem: Identifiable{
+    let id = UUID()
+    let NotebookTitles: String
+    let NotebookImages: String
+    let NotebookPrices: String
+}
+
+var NotebookItems = [
+    NotebookItem(NotebookTitles: "MacBookPro 14", NotebookImages: "Notebook", NotebookPrices: "₩2,690,000"),
+    NotebookItem(NotebookTitles: "MacBookPro 13", NotebookImages: "Notebook", NotebookPrices: "₩2,490,000"),
+    NotebookItem(NotebookTitles: "MacBookPro 16", NotebookImages: "Notebook", NotebookPrices: "₩3,090,000"),
+    NotebookItem(NotebookTitles: "MacBookAir 13", NotebookImages: "Notebook", NotebookPrices: "₩1,690,000"),
+    NotebookItem(NotebookTitles: "MacBookAir 13", NotebookImages: "Notebook", NotebookPrices: "₩1,690,000"),
+    NotebookItem(NotebookTitles: "MacBookPro 14", NotebookImages: "Notebook", NotebookPrices: "₩2,690,000"),
+    NotebookItem(NotebookTitles: "MacBookPro 16", NotebookImages: "Notebook", NotebookPrices: "₩3,790,000"),
+    NotebookItem(NotebookTitles: "MacBookPro 16", NotebookImages: "Notebook", NotebookPrices: "₩3,090,000"),
+]

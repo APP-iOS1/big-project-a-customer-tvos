579e5220e361c9eb572e86878ccf8acb9d1b3250
gxdxt <yxxsxxny@gmail.com>
2023-01-03 11:03:58 +0900

    [Feat] Firebase Package 추가

--- AuChulTV/AuChulTV/AuChulTVApp.swift
+++ AuChulTV/AuChulTV/AuChulTVApp.swift
@@ -4,15 +4,30 @@
 //
 //  Created by tae on 2023/01/02.
 //
-
 import SwiftUI
+import FirebaseCore
 
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct AuChulTVApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
